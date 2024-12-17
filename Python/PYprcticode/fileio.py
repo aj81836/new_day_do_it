a0c06f177044b2e720131841de7ca9473b3d834d
aj81836 <aj81836@gmail.com>
2024-12-17 23:17:46 +0530

    Python

---
+++ Python/PYprcticode/fileio.py
@@ -0,0 +1,17 @@
+# # # # f=open("data.txt","w+")
+# # # # f.write("abhijit ")
+# # # # print(f.read())
+# # # # f.close()
+
+# # # creat file use f=open("file name.txt","x")
+# # # f=open("demo.txt","w")
+# # # f.write("kon bany gaa KBC winner oooo")
+
+# # import os
+
+# # os.remove("data.txt")
+# # f=open("new file.txt","w")
+# # f.write("......................")
+
+# import os
+# os.remove("new file.txt")
--- Python/PYprcticode/loop02.py
+++ Python/PYprcticode/loop02.py
@@ -1,31 +1,45 @@
-# # # # seq=range(10)
+# # # # # # seq=range(10)
 
-# # # # for i in seq:
-# # # #     print(i)
+# # # # # # for i in seq:
+# # # # # #     print(i)
 
-# # # for i in range(2,10):
-# # #     print(i)
+# # # # # for i in range(2,10):
+# # # # #     print(i)
 
-# # n=7
+# # # # n=7
 
-# # sum=0
-# # for i in range(0,n+1):
-# #     sum+=i
+# # # # sum=0
+# # # # for i in range(0,n+1):
+# # # #     sum+=i
 
-# # print("total sum=",sum)
+# # # # print("total sum=",sum)
 
-# n=7
-# sum=0
-# i=1
-# while i<=n:
-#     sum+=i
-#     i+=1
+# # # n=7
+# # # sum=0
+# # # i=1
+# # # while i<=n:
+# # #     sum+=i
+# # #     i+=1
 
-# print("sum=",sum)
+# # # print("sum=",sum)
 
-n=5
-fact=1
-for i in range(1,n+1):
-    fact *=i
+# # n=5
+# # fact=1
+# # for i in range(1,n+1):
+# #     fact *=i
 
-print("factorial is: ",fact)
+# # print("factorial is: ",fact)
+
+# def cal_sum(a,b):
+#     return a+b
+
+# sum=cal_sum(2,7)
+# print(sum)
+
+def cal_sum(n):
+    if(n==0):
+        return 0
+    return cal_sum(n-1)+n
+
+sum=cal_sum(10)
+print(sum)
